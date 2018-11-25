48561780ef4c9459a76904563fbfb610dc9790b8
Worachit999 <worachit1@gmail.com>
2018-11-26 01:21:51 +0700

    add static and template

---
+++ vsite/templates/README.md
@@ -0,0 +1,1 @@
+# Vkids-Front-End
--- vsite/user/views.py
+++ vsite/user/views.py
@@ -3,5 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
-    return render(request,'user/register.html',{'form':form})
+    return render(request,'user/register.html')
+    # form = UserCreationForm()
+    # return render(request,'user/register.html',{'form' : form})
