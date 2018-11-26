37e1c97f976fe6133408f6ebee6e4ad9007fa142
Worachit999 <worachit1@gmail.com>
2018-11-26 21:03:39 +0700

    add login

--- vsite/home/urls.py
+++ vsite/home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('', views.home,name='vkids-home')
+    path('', views.home, name='vkids-home')
 ]
